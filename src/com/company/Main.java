311df25f9307b2d34540a7afc584895f85636e94
DomCS <domsakrisson@gmail.com>
2017-11-18 22:01:10 -0800

    this is my initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello woeld");
+        System.out.println("written in git");
+    }
+}
